4c7bbe8138ae6dde8d5edecdcfc7847d02940898
Andrei Rubaniuk <rubaniuk@gmail.com>
2023-10-07 18:20:42 -0700

    Intial check-in: deleting AAE, IMG_E, THM, etc.

--- PhotosCleanup.cmd
+++ PhotosCleanup.cmd
@@ -1,2 +1,25 @@
 @echo off
 echo Running PhotosCleanup script.
+echo.
+echo Deleting AAE files.
+del /s *.AAE
+
+echo.
+echo Deleting IMG_E*.jpg files (rotated copies of original photos, Apple iPhone).
+del /s IMG_E*.jpg
+
+echo.
+echo Deleting TMP files.
+del /s *.TMP
+
+echo.
+echo Deleting THM files.
+del /s *.THM
+
+echo.
+echo Deleting DTHUMB files.
+del /s *.DTHUMB
+
+echo.
+echo Deleting LNK files.
+del /s *.LNK
